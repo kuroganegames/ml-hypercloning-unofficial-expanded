9019fbcdf65a15ad7db5c294280ab6c9f0e43bee
kuroganegames <37547447+kuroganegames@users.noreply.github.com>
2026-07-02 23:39:29 +0900

    Make Mixtral package export lazy

--- hypercloning/models/mixtral/__init__.py
+++ hypercloning/models/mixtral/__init__.py
@@ -1,5 +1,14 @@
 """Mixtral HyperCloning integration."""
 
-from hypercloning.models.mixtral.cloning import clone_mixtral
+from __future__ import annotations
+
+
+def __getattr__(name: str):
+    if name == "clone_mixtral":
+        from hypercloning.models.mixtral.cloning import clone_mixtral
+
+        return clone_mixtral
+    raise AttributeError(name)
+
 
 __all__ = ["clone_mixtral"]
